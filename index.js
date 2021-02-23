ad3b98d950ec9de86c0d872a3550806b7c705bef
Rebecca Howe <Rebeccahowe20@gmail.com>
2021-02-23 07:12:30 -0800

    remove extra index file

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers/burgers_controller');
-
-const PORT = process.env || 8080;
-
-const app = express();
